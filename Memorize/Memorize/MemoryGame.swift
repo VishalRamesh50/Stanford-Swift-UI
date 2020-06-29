b55024aae9db490f8977f247638f5bb1b1f3a031
Vishal Ramesh <vishalramesh50@gmail.com>
2020-06-28 22:43:58 -0400

    Stub out MemoryGame model

---
+++ Memorize/Memorize/MemoryGame.swift
@@ -0,0 +1,23 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Vishal Ramesh on 6/28/20.
+//  Copyright © 2020 Vishal Ramesh. All rights reserved.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards: Array<Card>
+    
+    func chooseCard(card: Card) {
+        print("card chosen: \(card)")
+    }
+    
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: CardContent
+    }
+}
