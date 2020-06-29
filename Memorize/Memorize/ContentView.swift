a92737afc49ac5ffed0166ff226bef36fe1e7f1d
Vishal Ramesh <vishalramesh50@gmail.com>
2020-06-28 22:39:45 -0400

    Encapsulate the CardView in it's own struct
    - Also simplifies syntax to bare minimum

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -10,18 +10,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        return HStack(content: {
-            ForEach(0..<4, content: { index in
-                ZStack(content: {
-                    RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                    RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-                    Text("👻")
-                })
-            })
-        })
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
             .padding()
             .foregroundColor(Color.orange)
             .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            }
+            else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
     }
 }
 
