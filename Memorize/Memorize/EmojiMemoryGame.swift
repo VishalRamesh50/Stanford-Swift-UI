147fc1a5dcd77209cee2f5ea3c2279a857147359
Vishal Ramesh <vishalramesh50@gmail.com>
2020-06-28 23:15:30 -0400

    Create an EmojiMemoryGame which is a ViewModel

---
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,27 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Vishal Ramesh on 6/28/20.
+//  Copyright © 2020 Vishal Ramesh. All rights reserved.
+//
+
+import Foundation
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = MemoryGame<String>(numberOfPairsOfCards: 2, cardContentFactory: { (pairIndex: Int) -> String in
+        return "😀"
+    })
+    
+    // MARK: - Access to the Model
+
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intents(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
