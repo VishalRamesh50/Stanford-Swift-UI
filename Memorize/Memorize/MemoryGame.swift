ac508a63d042027ac4a0caaf8300829d0edd2f38
Vishal Ramesh <vishalramesh50@gmail.com>
2020-06-28 23:15:00 -0400

    Create an init for the MemoryGame

--- Memorize/Memorize/MemoryGame.swift
+++ Memorize/Memorize/MemoryGame.swift
@@ -11,6 +11,15 @@ import Foundation
 struct MemoryGame<CardContent> {
     var cards: Array<Card>
     
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = Array<Card>()
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(isFaceUp: false, isMatched: false, content: content))
+            cards.append(Card(isFaceUp: false, isMatched: false, content: content))
+        }
+    }
+    
     func choose(card: Card) {
         print("card chosen: \(card)")
     }
