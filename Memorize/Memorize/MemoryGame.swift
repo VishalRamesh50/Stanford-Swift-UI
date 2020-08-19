133cab499eedc8af6198023a30ca08307d77bd2d
Vishal Ramesh <vishalramesh50@gmail.com>
2020-08-19 16:18:09 -0400

    Extend Array functionality to implement repeating function
    - This function will get the firstIndex when it finds a match with the given Identifiable element.
    - Exending the array like this allows us to prevent duplicate code and use the arrays directly. It also makes more sense to put this code with the array since they don't need to be a part of the Grid or the MemoryGame.
    - It is common convention to name files which extend something as the thing+what addional functionality it's adding.

--- Memorize/Memorize/MemoryGame.swift
+++ Memorize/Memorize/MemoryGame.swift
@@ -23,17 +23,8 @@ struct MemoryGame<CardContent> {
     
     mutating func choose(card: Card) {
         print("card chosen: \(card)")
-        let chosenIndex: Int = self.index(of: card)
+        let chosenIndex: Int = cards.firstIndex(matching: card)
         self.cards[chosenIndex].isFaceUp = !self.cards[chosenIndex].isFaceUp
-    }
-    
-    func index(of card: Card) -> Int {
-        for index in 0..<self.cards.count {
-            if self.cards[index].id == card.id {
-                return index
-            }
-        }
-        return 0 // TODO: bogus!
     }
     
     struct Card: Identifiable {
