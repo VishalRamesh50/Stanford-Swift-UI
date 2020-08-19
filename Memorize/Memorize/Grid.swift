5c4f6b0aaa43f7fda418d5286fb7d83e3774cab0
Vishal Ramesh <vishalramesh50@gmail.com>
2020-08-19 15:59:45 -0400

    Use GeometryReader to get size
    Also breaks out each part into helper functions to avoid the use of self on each viewForItem

--- Memorize/Memorize/Grid.swift
+++ Memorize/Memorize/Grid.swift
@@ -16,9 +16,20 @@ struct Grid<Item, ItemView>: View where Item: Identifiable, ItemView: View {
         self.items = items
         self.viewForItem = viewForItem
     }
+    
     var body: some View {
-        ForEach(items) { item in
-            self.viewForItem(item)
+        GeometryReader { geometry in
+         self.body(for: geometry.size)
         }
+    }
+    
+    func body(for size: CGSize) -> some View {
+        ForEach(items) { item in
+            self.body(for: item, in: size)
+        }
+    }
+    
+    func body(for item: Item, in size: CGSize) -> some View {
+        return viewForItem(item)
     }
 }
