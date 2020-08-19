55350a41548b405fbf4d04fc9b71500e314c19ea
Vishal Ramesh <45186928+VishalRamesh50@users.noreply.github.com>
2020-08-19 13:18:24 -0400

    Lecture3 (#2)
    * Rename ContentView to EmojiMemoryGameView
    
    * Implement choose function
    
    - Since the card inside the function of choose is really just a copy the array itself needs to be modified.
    - To do this, the index where the card would be in the array is computed via an index function which is created.
    - Then that index is used to get the actual card from the array and flip it's isFaceUp attribute.
    - Functions which mutate must be explicitly declared so the keyword "mutating" is added before the func choose
    - Nothing visually happens yet on the screen when clicked because the view is not reactive yet (aka updating when the model updates with new info)
    
    * Make UI reactive to the model
    
    - @Published is a property wrapper which will ensure that everytime the model is changed it will call objectWillChange.send() which basically publishes a message saying the EmojiMemoryGame will change. Views looking at this game need to know this information to redraw themselves.
    - @ObservedObject is another property wrapper which basically says this item has an ObservableObject in it and any time objectWillChange.send() is fired, redraw. It smartly does this to only redraw the things that changed and does this using the Identifiable characteristic of the cards.
    
    * Use GeometryReader to set font size in a better way
    
    - This sets the font by keeping the content boxed into the smallest factor, either the width or height of the card and then applies a 75% modifier so it doesn't go to the very edge.
    
    * Attempt to pull out some of the constants
    
    NOTE: This code will not build
    - This fails because you would still need to reference all the constants using self
    - The next commit will fix this
    
    * Fix previous issue with ".self" references
    
    - This creates a function called body which returns the ZStack separate from the Geometry Reader and then that is called in the Geometry reader using self.body.
    - This is the recommended approach until the self issues are solved in future updates and it just looks a bit cleaner.
    
    * Create a helper function to simplify UI even more
    
    - Taking steps to simplify the UI code as much as possible is encourages because it could get arbitrarily complex.

---
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -0,0 +1,66 @@
+//
+//  EmojiMemoryGameView.swift
+//  Memorize
+//
+//  Created by Vishal Ramesh on 6/28/20.
+//  Copyright © 2020 Vishal Ramesh. All rights reserved.
+//
+
+import SwiftUI
+
+struct EmojiMemoryGameView: View {
+    @ObservedObject var viewModel: EmojiMemoryGame
+
+    var body: some View {
+        HStack {
+            ForEach(viewModel.cards) { card in
+                CardView(card: card).onTapGesture {
+                    self.viewModel.choose(card: card)
+                }
+            }
+        }
+            .padding()
+            .foregroundColor(Color.orange)
+            .aspectRatio(2/3, contentMode: .fit)
+    }
+}
+
+struct CardView: View {
+    var card: MemoryGame<String>.Card
+
+    var body: some View {
+        GeometryReader {geometry in
+            self.body(for: geometry.size)
+        }
+    }
+    
+    func body(for size: CGSize) -> some View {
+        ZStack {
+            if card.isFaceUp {
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                Text(card.content)
+            }
+            else {
+                RoundedRectangle(cornerRadius: cornerRadius).fill()
+            }
+        }
+        .font(Font.system(size: fontSize(for: size)))
+    }
+    
+    // MARK: - Drawing Constants
+    let cornerRadius: CGFloat = 10.0
+    let edgeLineWidth: CGFloat = 3
+    func fontSize(for size: CGSize) -> CGFloat {
+        min(size.width, size.height) * 0.75
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            EmojiMemoryGameView(viewModel: EmojiMemoryGame()).colorScheme(.light)
+            EmojiMemoryGameView(viewModel: EmojiMemoryGame()).colorScheme(.dark)
+        }
+    }
+}
