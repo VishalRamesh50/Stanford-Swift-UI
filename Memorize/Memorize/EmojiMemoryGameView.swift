253df1bc769f07433cd517342770bb2f82f7d251
Vishal Ramesh <vishalramesh50@gmail.com>
2020-08-19 00:20:52 -0400

    Use GeometryReader to set font size in a better way
    - This sets the font by keeping the content boxed into the smallest factor, either the width or height of the card and then applies a 75% modifier so it doesn't go to the very edge.

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -21,7 +21,6 @@ struct EmojiMemoryGameView: View {
         }
             .padding()
             .foregroundColor(Color.orange)
-            .font(viewModel.cards.count / 2 < 5 ? Font.largeTitle: Font.body)
             .aspectRatio(2/3, contentMode: .fit)
     }
 }
@@ -30,15 +29,18 @@ struct CardView: View {
     var card: MemoryGame<String>.Card
 
     var body: some View {
-        ZStack {
-            if card.isFaceUp {
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-                Text(card.content)
+        GeometryReader {geometry in
+            ZStack {
+                if self.card.isFaceUp {
+                    RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                    RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                    Text(self.card.content)
+                }
+                else {
+                    RoundedRectangle(cornerRadius: 10.0).fill()
+                }
             }
-            else {
-                RoundedRectangle(cornerRadius: 10.0).fill()
-            }
+            .font(Font.system(size: min(geometry.size.width, geometry.size.height) * 0.75))
         }
     }
 }
