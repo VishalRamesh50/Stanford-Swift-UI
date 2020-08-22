7c0a9ad089907f9bb021af1627b888f257fc96f2
Vishal Ramesh <vishalramesh50@gmail.com>
2020-08-22 17:24:46 -0400

    Add circle behind each card icon
    - This gets rid of the dark theme preview since it doesn't seem to work anyways.
    - Also pre-renders the canvas with the first card selected to easily see how it looks as I work on it.
    - Creates a semi-transparent circle to place behind the icon which will be the base to starting the ticking down circle as time goes on.

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -59,6 +59,7 @@ struct CardView: View {
             if card.isFaceUp {
                 RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
                 RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                Circle().padding(5).opacity(0.4)
                 Text(card.content)
             }
             else {
@@ -75,15 +76,14 @@ struct CardView: View {
     private let cornerRadius: CGFloat = 10.0
     private let edgeLineWidth: CGFloat = 3
     private func fontSize(for size: CGSize) -> CGFloat {
-        min(size.width, size.height) * 0.75
+        min(size.width, size.height) * 0.7
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
-            EmojiMemoryGameView(viewModel: EmojiMemoryGame()).colorScheme(.light)
-            EmojiMemoryGameView(viewModel: EmojiMemoryGame()).colorScheme(.dark)
-        }
+        let game = EmojiMemoryGame()
+        game.choose(card: game.cards[0])
+        return EmojiMemoryGameView(viewModel: game)
     }
 }
