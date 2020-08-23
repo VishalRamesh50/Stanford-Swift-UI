58ac9d073a94eabc1d173fecfc293db84dcb4c3b
Vishal Ramesh <vishalramesh50@gmail.com>
2020-08-23 00:40:34 -0400

    Kick of the bonusTime functions using property observers
    - These property observers provide a powerful and reliable way to start an action rather than sprinkling it throughout the choose function and hoping you don't forget it somewhere.
    - This also now keeps track of the bonus time so it can be used it other places.

--- Memorize/Memorize/MemoryGame.swift
+++ Memorize/Memorize/MemoryGame.swift
@@ -57,8 +57,20 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     }
     
     struct Card: Identifiable {
-        var isFaceUp: Bool = false
-        var isMatched: Bool = false
+        var isFaceUp: Bool = false {
+            didSet {
+                if isFaceUp {
+                    startUsingBonusTime()
+                } else {
+                    stopUsingBonusTime()
+                }
+            }
+        }
+        var isMatched: Bool = false {
+            didSet {
+                stopUsingBonusTime()
+            }
+        }
         var content: CardContent
         var id: Int
         var color: ThemeColor
