1a5b6557fe40211aa5a6d7363e1fdc033f87ebdb
Vishal Ramesh <vishalramesh50@gmail.com>
2020-08-22 23:29:16 -0400

    Implicit Animation - Rotate Forever When Matched
    - This will rotate the matched cards 360 degrees forever when they are matched.
    - This doesn't animate both cards when they are matched but this will be fixed later.

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -60,7 +60,10 @@ struct CardView: View {
             ZStack {
                 Pie(startAngle: Angle.degrees(0-90), endAngle:Angle.degrees(110-90), clockwise: true)
                     .padding(5).opacity(0.4)
-                Text(card.content).font(Font.system(size: fontSize(for: size)))
+                Text(card.content)
+                    .font(Font.system(size: fontSize(for: size)))
+                    .rotationEffect(Angle.degrees(card.isMatched ? 360: 0))
+                    .animation(card.isMatched ? Animation.linear(duration: 1).repeatForever(autoreverses: false): .default)
             }
             .cardify(isFaceUp: card.isFaceUp)
             .foregroundColor(Color.from(themeColor: card.color))
