a4d035b5fe825b1768ca5bbbeda0151d9d1e0f07
Vishal Ramesh <vishalramesh50@gmail.com>
2020-08-22 20:53:15 -0400

    Create Cardify ViewModifier
    - This is a ViewModifier like .padding(), .font(), etc.
    - It's meant to turn any Content into a Card-Like View.
    - But it should hold no game specific features like matching in case you want to use it in a more general case. This is why this is taken out of this modifier.
    - However, this also means that matched cards will not be removed from the board, so this will be fixed in the next commit.

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -56,26 +56,15 @@ struct CardView: View {
     
     private func body(for size: CGSize) -> some View {
         ZStack {
-            if card.isFaceUp {
-                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
-                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
-                Pie(startAngle: Angle.degrees(0-90), endAngle:Angle.degrees(110-90), clockwise: true)
-                    .padding(5).opacity(0.4)
-                Text(card.content)
-            }
-            else {
-                if !card.isMatched {
-                    RoundedRectangle(cornerRadius: cornerRadius).fill()
-                }
-            }
+            Pie(startAngle: Angle.degrees(0-90), endAngle:Angle.degrees(110-90), clockwise: true)
+                .padding(5).opacity(0.4)
+            Text(card.content).font(Font.system(size: fontSize(for: size)))
         }
-        .font(Font.system(size: fontSize(for: size)))
+        .cardify(isFaceUp: false)
         .foregroundColor(Color.fromThemeColor(color: card.color))
     }
     
     // MARK: - Drawing Constants
-    private let cornerRadius: CGFloat = 10.0
-    private let edgeLineWidth: CGFloat = 3
     private func fontSize(for size: CGSize) -> CGFloat {
         min(size.width, size.height) * 0.7
     }
