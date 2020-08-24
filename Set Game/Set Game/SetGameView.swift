9ac195307ec75a268478911f4141a15220b45b06
Vishal Ramesh <vishalramesh50@gmail.com>
2020-08-24 18:12:20 -0400

    Put ViewModel in View

--- Set Game/Set Game/SetGameView.swift
+++ Set Game/Set Game/SetGameView.swift
@@ -9,6 +9,8 @@
 import SwiftUI
 
 struct SetGameView: View {
+    @ObservedObject var viewModel: SetGameViewModel
+
     var body: some View {
         Text("Hello, World!")
     }
@@ -16,6 +18,6 @@ struct SetGameView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        SetGameView()
+        SetGameView(viewModel: SetGameViewModel())
     }
 }
