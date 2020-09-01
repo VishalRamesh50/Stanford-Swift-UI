b2d690329410db2173a86b1dc7c96093a078a9b8
Vishal Ramesh <vishalramesh50@gmail.com>
2020-09-01 14:26:09 -0400

    Start new project and stub out View and ViewModel

---
+++ EmojiArt/EmojiArt/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by Vishal Ramesh on 8/31/20.
+//  Copyright © 2020 Vishal Ramesh. All rights reserved.
+//
+
+import SwiftUI
+
+struct EmojiArtDocumentView: View {
+    @ObservedObject var document: EmojiArtDocument
+
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
---
+++ EmojiArt/EmojiArt/EmojiArtDocument.swift
@@ -0,0 +1,13 @@
+//
+//  EmojiArtDocument.swift
+//  EmojiArt
+//
+//  Created by Vishal Ramesh on 9/1/20.
+//  Copyright © 2020 Vishal Ramesh. All rights reserved.
+//
+
+import SwiftUI
+
+class EmojiArtDocument: ObservableObject {
+    
+}
