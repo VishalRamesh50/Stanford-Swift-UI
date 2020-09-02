eeff22ed00297ef7bd6a1f52ac992af5446afc38
Vishal Ramesh <vishalramesh50@gmail.com>
2020-09-02 19:42:21 -0400

    Make it so that double tap gestures will zoom to fit the image regardless of size
    - It also animates so that the change is not jarring.
    - However, there is an issue because the emojis don't animate with the background. They jump to the size they need to be and even get a little cut off as the View is adjusting before it knows what size to be.

--- EmojiArt/EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArt/EmojiArtDocumentView.swift
@@ -28,6 +28,7 @@ struct EmojiArtDocumentView: View {
                         OptionalImage(uiImage: self.document.backgroundImage)
                             .scaleEffect(self.zoomScale)
                     )
+                        .gesture(self.doubleTapToZoom(in: geometry.size))
                     ForEach(self.document.emojis) { emoji in
                         Text(emoji.text)
                             .font(self.font(for: emoji))
@@ -49,6 +50,23 @@ struct EmojiArtDocumentView: View {
     }
     
     @State private var zoomScale: CGFloat = 1.0
+    
+    private func doubleTapToZoom(in size: CGSize) -> some Gesture {
+        TapGesture(count: 2)
+            .onEnded {
+                withAnimation {
+                    self.zoomToFit(self.document.backgroundImage, in: size)
+                }
+            }
+    }
+    
+    private func zoomToFit(_ image: UIImage?, in size: CGSize) {
+        if let image = image, image.size.width > 0, image.size.height > 0 {
+            let hZoom = size.width / image.size.width
+            let vZoom = size.height / image.size.height
+            self.zoomScale = min(hZoom, vZoom)
+        }
+    }
     
     private func font(for emoji: EmojiArt.Emoji) -> Font {
         Font.system(size: emoji.fontSize * self.zoomScale)
